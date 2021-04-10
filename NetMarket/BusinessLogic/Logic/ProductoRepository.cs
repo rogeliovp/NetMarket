5f1873a22fb3af39c26cf98343fb236cfe63763b
Rogelio <rogelio.vilches@gmail.com>
2021-04-10 11:27:41 -0400

    Modificando webapi1

--- NetMarket/BusinessLogic/Logic/ProductoRepository.cs
+++ NetMarket/BusinessLogic/Logic/ProductoRepository.cs
@@ -1,5 +1,7 @@
-﻿using Core.Entities;
+﻿using BusinessLogic.Data;
+using Core.Entities;
 using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +12,21 @@ namespace BusinessLogic.Logic
 {
     public class ProductoRepository : IProductoRepository
     {
-        public Task<Producto> GetProductoByIdAsync(int id)
+        private readonly MarketDbContext _context;
+
+        public ProductoRepository(MarketDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task<IReadOnlyList<Producto>> GetProductosAsync()
+        public async Task<Producto> GetProductoByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _context.Producto.FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<Producto>> GetProductosAsync()
+        {
+            return await _context.Producto.ToListAsync();
         }
     }
 }
