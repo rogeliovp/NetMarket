43786633b106d08ee9fc18f4caa4961d3ddc2ebb
Rogelio <rogelio.vilches@gmail.com>
2021-04-11 11:55:30 -0400

    Se agrega proyecto WebAPI con controlador de Productos

--- NetMarket/BusinessLogic/Logic/ProductoRepository.cs
+++ NetMarket/BusinessLogic/Logic/ProductoRepository.cs
@@ -21,12 +21,18 @@ namespace BusinessLogic.Logic
 
         public async Task<Producto> GetProductoByIdAsync(int id)
         {
-            return await _context.Producto.FindAsync(id);
+            return await _context.Producto
+                .Include(p => p.Marca)
+                .Include(p => p.Categoria)
+                .FirstOrDefaultAsync(p => p.Id == id);
         }
 
         public async Task<IReadOnlyList<Producto>> GetProductosAsync()
         {
-            return await _context.Producto.ToListAsync();
+            return await _context.Producto
+                .Include(p => p.Marca)
+                .Include(p => p.Categoria)
+                .ToListAsync();
         }
     }
 }
---
+++ NetMarket/WebAPI_back/Controllers/ProductoController.cs
@@ -0,0 +1,36 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private readonly IProductoRepository _productoRepository;
+
+        public ProductoController(IProductoRepository productoRepository)
+        {
+            _productoRepository = productoRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Producto>>> GetProductos()
+        {
+            var productos = await _productoRepository.GetProductosAsync();
+            return Ok(productos);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Producto>> GetProducto(int id)
+        {
+            return await _productoRepository.GetProductoByIdAsync(id);
+        }
+    }
+}
